b46cb0667a0d935de3fd62591ea71d7d3f91ea9f
prusche <prusche@gmail.com>
2017-04-16 04:19:54 -0700

    add book titles solution

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,26 @@
 class Book
 # write your code here
+  attr_accessor :title
+
+  def title
+    @title = titleize(@title)
+  end
+
+  private
+  def titleize str
+    new_arr = []
+    arr = str.split
+    arr.each do |i|
+      if i == "and" || i == "over" || i == "the" || i == "in" || i == "of" || i == "a" || i == "an"
+        new_arr.push(i)
+      else
+        new_arr.push(i.capitalize)
+      end
+    end
+    new_arr[0].capitalize!
+    return new_arr.join(' ')
+  end
+
+
+
 end
